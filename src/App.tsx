e196065b60e60f2b5f49a03f7216503474e6c0d2
kupre1 <160654812+kupre1@users.noreply.github.com>
2025-01-24 16:12:43 +0400

    feat(HEADER):create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Invoice from './pages/Invoice';
+"use client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import { Invoice } from "./pages/Invoice";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/:id' element={<Invoice />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Invoice />} />
       </Routes>
     </BrowserRouter>
   );
