1c38c4a8f94fa251c34c0e1d10e2ceaba6b8b183
nikakvinikadze <nikakvinikadze2007@gmail.com>
2025-01-26 15:33:29 +0400

    feature/new invoice

--- src/components/NewInvoice.tsx
+++ src/components/NewInvoice.tsx
@@ -6,14 +6,12 @@ export default function NewInvoice({
   setShowNewInvoice: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   return (
-    <div className='h-screen w-screen'>
-      {/* Invoice form container */}
-      <div className='absolute top-0 left-0 h-full w-full bg-white tablet:max-w-[616px] desktop:max-w-[719px] z-20'>
+    <div className='absolute inset-0 z-40'>
+      <div className='absolute h-full w-full overflow-scroll bg-white tablet:max-w-[616px] desktop:max-w-[719px]'>
         <h1 className='text-mainDark font-bold text-[24px] leading-8 mb-[22px] tablet:mb-[46px]'>
           New Invoice
         </h1>
         <InvoiceForm setShowNewInvoice={setShowNewInvoice} />
-        {/* Invoice buttons */}
       </div>
     </div>
   );
