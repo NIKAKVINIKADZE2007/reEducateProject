99d01ad532440cdfc8967b12f697097e16b74e1e
nikakvinikadze <nikakvinikadze2007@gmail.com>
2025-01-27 21:10:37 +0400

    fix

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,4 @@
-import { Logo, moon, sun, pfp, circle } from "../assets";
+import { Logo, moon, pfp, circle } from '../assets';
 
 type SideBarProps = {
   isLight: boolean;
@@ -12,20 +12,20 @@ export default function SideBar({ isLight, setIsLight }: SideBarProps) {
   return (
     <div
       className={`w-full h-[72px] fixed top-0 z-50 flex  justify-between bg-[#373B53] tablet:h-[80px] desktop:flex-col desktop:h-screen desktop:w-[103px] desktop:rounded-r-[20px] ${
-        isLight ? "bg-[#373B53]" : "bg-[#1E2139]"
+        isLight ? 'bg-[#373B53]' : 'bg-[#1E2139]'
       }  `}
     >
-      <div className="size-[72px] flex justify-center items-center rounded-r-[20px] bg-purpleDark tablet:size-[80px] desktop:size-[103px]">
-        <img className="h-[28px] w-[28px]" src={Logo} alt="logo" />
+      <div className='size-[72px] flex justify-center items-center rounded-r-[20px] bg-purpleDark tablet:size-[80px] desktop:size-[103px]'>
+        <img className='h-[28px] w-[28px]' src={Logo} alt='logo' />
       </div>
-      <div className="cursor-pointer w-full max-w-[101px] mr-[24px] flex justify-between relative items-center tablet:mr-[32px] desktop:mr-0 desktop:mb-8 desktop:flex-col desktop:w-full desktop:h-[103px]">
+      <div className='cursor-pointer w-full max-w-[101px] mr-[24px] flex justify-between relative items-center tablet:mr-[32px] desktop:mr-0 desktop:mb-8 desktop:flex-col desktop:w-full desktop:h-[103px]'>
         <img
           src={isLight ? moon : circle}
-          alt="moon"
+          alt='moon'
           onClick={handleLightClick}
         />
-        <div className="absolute right-[50%] top-0 bg-[#494E6E] w-[1px] h-full desktop:h-[1px] desktop:w-full desktop:right-0 desktop:top-[50%]" />
-        <img src={pfp} className="rounded-[50%] size-8" alt="profile-picture" />
+        <div className='absolute right-[50%] top-0 bg-[#494E6E] w-[1px] h-full desktop:h-[1px] desktop:w-full desktop:right-0 desktop:top-[50%]' />
+        <img src={pfp} className='rounded-[50%] size-8' alt='profile-picture' />
       </div>
     </div>
   );
